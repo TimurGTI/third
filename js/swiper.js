830a7cc9c41100ad3bddf3542d993a437d454875
TimurGTI <126509011+TimurGTI@users.noreply.github.com>
2024-10-12 20:30:00 +0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,40 @@
+const swiperMedia = window.matchMedia("(max-width: 768px)");
+
+function swiperInit(swiperMedia) {
+  if (swiperMedia.matches) { 
+    const brandsSwiper = new Swiper('.swiper-main', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      },
+      spaceBetween: 16,
+      slidesPerView: 'auto',
+      slidesOffsetAfter: 16,
+      slidesOffsetBefore: 16,
+    }); 
+  } 
+}
+
+swiperInit(swiperMedia);
+
+const brands = document.querySelector('.swiper-wrapper');
+const openButton = document.querySelector('.swiper-button');
+
+openButton.addEventListener('click', function() {
+  brands.classList.toggle('swiper-main__wrapper--hidden');
+  openButton.classList.toggle('swiper-button--open');
+  if (brands.classList.contains('swiper-main__wrapper--hidden')) {
+    openButton.textContent = 'Показать все';
+  } else  {
+    openButton.textContent = 'Скрыть'
+  }
+  
+});
+
+
+
+
+
+
+
+  
